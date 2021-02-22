858e382c200c251d99219d65de8f806990bd125e
rishichawda <chawda.rishii@gmail.com>
2021-02-22 17:56:18 +0530

    docs(iam): added more comments
    Signed-off-by: rishichawda <chawda.rishii@gmail.com>

--- aws/modules/iam/main.tf
+++ aws/modules/iam/main.tf
@@ -1,3 +1,5 @@
+# Let terraform create the policy document and use the JSON generate from it.
+# Creating a policy document file manually and using its content is somewhat supported, but not recommended.
 data "aws_iam_policy_document" "role_policy_document" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -9,11 +11,14 @@ data "aws_iam_policy_document" "role_policy_document" {
   }
 }
 
+# Create IAM role
 resource "aws_iam_role" "s3_access_role" {
   name = "cdqs-s3-access-iam-role"
   assume_role_policy = data.aws_iam_policy_document.role_policy_document.json
 }
 
+# Let terraform create the policy document and use the JSON generate from it.
+# Creating a policy document file manually and using its content is somewhat supported, but not recommended.
 data "aws_iam_policy_document" "policy_document" {
   statement {
     # Allows all actions on s3
@@ -23,11 +28,13 @@ data "aws_iam_policy_document" "policy_document" {
   }
 }
 
+# Create IAM policy
 resource "aws_iam_policy" "access_policy" {
   name = "cdqs-access-policy"
   policy = data.aws_iam_policy_document.policy_document.json
 }
 
+# Assign the policy to role.
 resource "aws_iam_policy_attachment" "access_policy_attachment" {
   name = "cdqs-access-policy-attachment"
   roles = [ aws_iam_role.s3_access_role.name ]
